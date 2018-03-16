08a62999bdb69cd35bc8c0abfea12dcf1613d22f
HideakiTouhara <hideee6723@gmail.com>
2018-03-16 23:02:03 +0900

    Initial commit

---
+++ Assets/bird.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour {
+
+	public GameObject panelGameOver;
+    void OnCollisionEnter2D (Collision2D coll) {
+        panelGameOver.SetActive (true);
+    }
+	public void fly() {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 1f);
+	}
+}
---
+++ Assets/clear.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clear : MonoBehaviour {
+
+	public GameObject panel;
+	void OnCollisionEnter2D(Collision2D coll) {
+		panel.SetActive(true);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
